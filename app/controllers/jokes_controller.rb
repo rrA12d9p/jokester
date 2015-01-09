75fd990dadc0757c15d93752c15c4a4f999b5b81
TREY <rkh.mymail@gmail.com>
2015-01-09 13:18:25 -0500

    change joke show route to use relative user_joke id instead of joke_id

--- app/controllers/jokes_controller.rb
+++ app/controllers/jokes_controller.rb
@@ -3,8 +3,13 @@ class JokesController < ApplicationController
 		@jokes=Joke.order(updated_at: :desc)
 	end
 	def show
-		@joke = Joke.find(params[:id])
+		# find the joke relative to a user, not all jokes
+		# i.e. /users/5/jokes/5 is the default for user 5's
+		# first joke when there were 4 other users with jokes already
+		# instead, we want /users/5/jokes/1 to show the user's first joke
+		
+		@user_joke_id = params[:user_joke_id].to_i
+		@user = User.find(params[:user_id])
+		@joke = @user.jokes.order(created_at: :asc).offset(@user_joke_id-1).first
 	end
-
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
  
   resources :users do 
-    resources :jokes 
+    resources :jokes, except: [:show]
+    resources :jokes, only: [:show], param: :user_joke_id
     resources :followers, only: [:create, :destroy, :index]
-
   end
 
   get '/jokes/', to: 'jokes#index', as: 'all_jokes'
