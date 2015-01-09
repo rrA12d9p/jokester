71b6ac381793be5fd345040731864acf9b909617
Kurt Weber <kdweber89@gmail.com>
2015-01-09 12:29:40 -0500

    working on routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,8 @@ Rails.application.routes.draw do
     resources :followers, only: [:create, :destroy, :index]
 
   end
+
+  get '/jokes/', to: 'jokes#index', as: 'all_jokes'
+  root 'jokes#index'
+
  end
