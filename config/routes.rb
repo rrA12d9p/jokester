9d628670d6479075da9ee8f419e891b5013bb8a3
Kurt Weber <kdweber89@gmail.com>
2015-01-09 12:04:11 -0500

    fixed followers destroy issue

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,7 @@ Rails.application.routes.draw do
  
   resources :users do 
     resources :jokes 
-    resources :followers, only: [ :create, :delete, :index]
+    resources :followers, only: [:create, :destroy, :index]
 
   end
-
-
-
  end
